b752dbb0885530692b7daaf2a2976e27a8f70d21
Nate Folkert <nfolkert@foursquare.com>
2011-08-24 13:59:29 -0400

    There is no userless token; use client key and secret

--- src/test/scala/org/scalafoursquare/ConnectTest.scala
+++ src/test/scala/org/scalafoursquare/ConnectTest.scala
@@ -6,7 +6,6 @@ import net.liftweb.util.Props
 
 class ConnectTest {
 
-  val USERLESS_TOKEN = Props.get("access.token.userless").open_!
   val USER_TOKEN = Props.get("access.token.user").open_!
   val CONSUMER_KEY = Props.get("consumer.key").open_!
   val CONSUMER_SECRET = Props.get("consumer.secret").open_!
@@ -17,9 +16,7 @@ class ConnectTest {
   @Test
   def simpleTest() {
 
-    println(List(USERLESS_TOKEN, USER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, TEST_URL, API_VERSION))
-
-    val json = App(CONSUMER_KEY, CONSUMER_SECRET, API_VERSION, TEST_URL).call(USERLESS_TOKEN).venueCategories.get
+    val json = App(CONSUMER_KEY, CONSUMER_SECRET, API_VERSION, TEST_URL).call(USER_TOKEN).venueCategories.get
     println(json)
   }
 
